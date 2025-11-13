0d4e5023dbafce2fb943e1d325d6a618f743a882
seilahhh <pereirajoaquim312@gmail.com>
2025-11-13 21:52:55 +0000

    Add websockets folder

---
+++ websockets/src/index.js
@@ -0,0 +1,13 @@
+import { Server } from "socket.io";
+
+const io = new Server(3000, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("echo", (msg) => {
+    socket.emit("echo", msg);
+  });
+});
